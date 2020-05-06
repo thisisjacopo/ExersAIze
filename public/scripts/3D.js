2bd9cef1bd5e2e23ace95d35f94763db654598da
Jacopo <jacopo.rodighiero@icloud.com>
2020-05-06 14:47:22 +0200

    merge

--- public/scripts/3D.js
+++ public/scripts/3D.js
@@ -1,51 +1,51 @@
-let cat,plant,sofa, table;
+let cat, plant, sofa, table;
+let start;
 
 function preload() {
   // Load model with normalise parameter set to true
-  cat = loadModel('/resources/cat.obj', true);
-    plant = loadModel('/resources/plant.obj', true);
-  sofa = loadModel('/resources/sofa.obj', true);
-  table = loadModel('/resources/table.obj', true);
-
+  cat = loadModel("/resources/cat.obj", true);
+  plant = loadModel("/resources/plant.obj", true);
+  sofa = loadModel("/resources/sofa.obj", true);
+  table = loadModel("/resources/table.obj", true);
 }
+loading();
 
 function setup() {
- let cv =  createCanvas(windowWidth, windowHeight, WEBGL);
- cv.position(0,0)
- cv.style('z-index','20')
+  let cv = createCanvas(windowWidth, windowHeight, WEBGL);
+  cv.position(0, 0);
+  cv.style("z-index", "20");
 }
 
 function draw() {
   background(30);
-  translate(-20, -100, mouseX/6)
+  translate(-20, -100, mouseX / 6);
   scale(0.9); // Scaled to make model fit into canvas
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   normalMaterial(); // For effect
   model(cat);
-  translate(120, 200, 83)
-   scale(1.4); // Scaled to make model fit into canvas
-   rotateZ(frameCount * 0.01)
+  translate(120, 200, 83);
+  scale(1.4); // Scaled to make model fit into canvas
+  rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.02);
   rotateY(frameCount * 0.01);
-  model(plant)
-  
-  
-    translate(110, 400, 113)
-   scale(0.6); // Scaled to make model fit into canvas
-   rotateZ(frameCount * -0.02)
+  model(plant);
+
+  translate(110, 400, 113);
+  scale(0.6); // Scaled to make model fit into canvas
+  rotateZ(frameCount * -0.02);
   rotateX(frameCount * 0.02);
   rotateY(frameCount * 0.01);
-  model(sofa)
-  
-    translate(410, 100, 713)
-   scale(0.6); // Scaled to make model fit into canvas
-   rotateZ(frameCount * 0.002)
+  model(sofa);
+
+  translate(410, 100, 713);
+  scale(0.6); // Scaled to make model fit into canvas
+  rotateZ(frameCount * 0.002);
   rotateX(frameCount * 0.001);
   rotateY(frameCount * 0.002);
-  model(table)
+  model(table);
 }
 
 function mousePressed() {
   window.location.href = "/main";
- }
+}
